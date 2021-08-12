f320a73d7d9437c79c7471fba3f26717032be49f
Caio Tieghi <ctieghi@rv3ti.com>
2021-08-12 00:25:53 -0300

    fix: configuração ds toques

--- src/toques.js
+++ src/toques.js
@@ -71,7 +71,7 @@ const ijexa = {
         {
             "nome": "Variação 1",
             "onomatopeia": "TA-1 TA-2/ TUM-3 TUM-4 TA-5/",
-            "imagem":ijexaV1
+            "imagem": ijexaV1
         },
         {
             "nome": "Variação 2",
@@ -96,15 +96,111 @@ const ijexa = {
     ]
 }
 
-export default function buscarToque(toque){
+const congo = {
+    "nome": "Congo",
+    "variacoes": [
+        {
+            "nome": "Base",
+            "onomatopeia": "TA-1 TA-2/ TA-3 TUM-4 TUM-5/ TA-6 TA-7 TA-8 TA-9/ TUM-10 TUM-11/",
+            "imagem": congoBase
+        },
+        {
+            "nome": "Encerramento",
+            "onomatopeia": "TA 1/ TA 2/ TA 3 – TRUM 4 TUM 5 TA 6/",
+            "imagem": congoEncerramento
+        },
+        {
+            "nome": "Variação 1",
+            "onomatopeia": "TA-1 TA-2/ TA-3 TUM-4 TUM-5/ TA-6 TA-7/ TUM-8 TUM-9/",
+            "imagem": congoV1
+        },
+        {
+            "nome": "Variação 2",
+            "onomatopeia": "TA-1 TA-2/ TA-3 TUM-4 TUM-5/ TA-6 TA-7/ TUM-8 TUM-9/",
+            "imagem": congoV2
+        },
+        {
+            "nome": "Variação 3",
+            "onomatopeia": "TAK-1 TA-2 TUM-3/ TA-4 TA-5/ TUM-6 TUM-7/",
+            "imagem": congoV3
+        },
+        {
+            "nome": "Variação 4",
+            "onomatopeia": "TA-1 TA-2 TUM-3 TUM-4 TA-5/ TUM-6 TUM-7/",
+            "imagem": congoV4
+        },
+        {
+            "nome": "Variação 5",
+            "onomatopeia": "TA-1 TA-2 TUM-3 TUM-4 TUM-5 TUM-6 TA-7/ TUM-8 TUM-9/",
+            "imagem": congoV5
+        },
+        {
+            "nome": "Variação 6",
+            "onomatopeia": "4X - TA-1 TUM-2 TUM-3/",
+            "imagem": congoV6
+        },
+        {
+            "nome": "Variação 7",
+            "onomatopeia": "4X - TA-1 TA-2 TA-3/",
+            "imagem": congoV7
+        },
+        {
+            "nome": "Variação 8",
+            "onomatopeia": "2X - TA-1 TA-2 TA-3/ 2X - TA-1 TUM-2 TUM-3/",
+            "imagem": congoV8
+        },
+        {
+            "nome": "Variação 9",
+            "onomatopeia": "2X - TA-1 TUM-2 TUM-3/ 2X - TA-1 TA-2 TA-3/",
+            "imagem": congoV9
+        },
+        {
+            "nome": "Variação 10",
+            "onomatopeia": "TA-1 TA-2 TA-3/ TA-4 TUM-5 TUM-6/",
+            "imagem": congoV10
+        },
+        {
+            "nome": "Variação 11",
+            "onomatopeia": "TA-1 TUM-2 TUM-3/ TA-4 TA-5 TA-6/",
+            "imagem": congoV11
+        },
+        {
+            "nome": "Variação 12",
+            "onomatopeia": "TA-1 TA-2 TA-3/ TA-4 TUM-5 TUM-6/ TA-1 TUM-2 TUM-3/ TA-4 TA-5 TA-6/",
+            "imagem": congoV12
+        },
+        {
+            "nome": "Variação 13",
+            "onomatopeia": "TA-1 TUM-2 TUM-3/ TA-4 TA-5 TA-6/ TA-1 TA-2 TA-3/ TA-4 TUM-5 TUM-6/",
+            "imagem": congoV13
+        },
+    ]
+}
+
+export default function buscarToque(toque) {
     let ret = {};
 
-    if(toque === 'barravento'){
+    if (toque === 'barravento') {
         ret = barravento;
     }
-    if(toque === 'ijexa'){
+    if (toque === 'ijexa') {
         ret = ijexa;
     }
+
+    switch (toque) {
+        case 'barravento':
+            ret = barravento;
+            break;
+        case 'ijexa':
+            ret = ijexa;
+            break;
+        case 'congo':
+            ret = congo;
+            break;
+        default:
+            console.log(`Sorry, we are out of ${expr}.`);
+    }
+
 
     return ret;
 }
